afd247737a66b41853ceb82778b6f50c01eed39e
Veronica Williams <veronica.robin.williams@gmail.com>
2016-04-18 12:42:45 -0400

    lab

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-var temp = 32
-var conversion = (temp - 32) * 5.9
-console.log (conversion)
+
+//lab 5 Veronica Williams
+
+var temp = 32; 
+
+
+console.log ((temp - 32) * 5/9 + " degrees celsius");
+
+
+
+
+//bonus
+var temp = 0; 
+var unit = "C";
+
+if (unit === "F") { 
+	console.log ((temp - 32) * 5/9 + " degrees celsius");
+}
+
+else   {
+	console.log ((temp * 1.8) + 32 +" degrees fahrenheit");
+}
+
 
